7d791d67e54075aa0ceee0c6f78968801541824f
GingerDog <GingerDog@a1433add-5e2c-0410-b055-b7f2511e0802>
2009-03-19 20:09:18 +0000

    branch to 2.3rc2
    git-svn-id: svn://svn.code.sf.net/p/postfixadmin/code@595 a1433add-5e2c-0410-b055-b7f2511e0802

---
+++ branches/postfixadmin-2.3rc2/users/password.php
@@ -0,0 +1,83 @@
+<?php
+/** 
+ * Postfix Admin 
+ * 
+ * LICENSE 
+ * This source file is subject to the GPL license that is bundled with  
+ * this package in the file LICENSE.TXT. 
+ * 
+ * Further details on the project are available at : 
+ *     http://www.postfixadmin.com or http://postfixadmin.sf.net 
+ * 
+ * @version $Id$ 
+ * @license GNU GPL v2 or later. 
+ * 
+ * File: password.php
+ * Used by users to change their mailbox (and login) password.
+ * Template File: users_password.php
+ *
+ * Template Variables:
+ *
+ * tMessage
+ *
+ * Form POST \ GET Variables:
+ *
+ * fPassword_current
+ * fPassword
+ * fPassword2
+ */
+
+require_once('../common.php');
+
+authentication_require_role('user');
+$USERID_USERNAME = authentication_get_username();
+
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    if(isset($_POST['fCancel'])) {
+        header("Location: main.php");
+        exit(0);
+    }
+
+    $fPassword_current = escape_string ($_POST['fPassword_current']);
+    $fPassword = escape_string ($_POST['fPassword']);
+    $fPassword2 = escape_string ($_POST['fPassword2']);
+
+    if(strlen($fPassword) < $CONF['min_password_length']) {
+        $error = 1;
+        flash_error(sprintf($PALANG['pPasswordTooShort'], $CONF['min_password_length']));
+    }
+    $username = $USERID_USERNAME;
+
+    $uh = new UserHandler();
+    if(!$uh->login($username, $fPassword_current)) {
+        $error += 1;
+        $pPassword_password_current_text = $PALANG['pPassword_password_current_text_error'];
+    }
+    if (empty ($fPassword) or ($fPassword != $fPassword2))
+    {
+        $error = 1;
+        $pPassword_password_text = $PALANG['pPassword_password_text_error'];
+    }
+
+    if ($error != 1)
+    {
+        if($uh->change_pass($username, $fPassword_current, $fPassword)) {
+            flash_info($PALANG['pPassword_result_success']);
+            header("Location: main.php");
+            exit(0);
+        }
+        else
+        {
+            $tMessage = $PALANG['pPassword_result_error'];
+        }
+    }
+}
+
+include ("../templates/header.php");
+include ("../templates/users_menu.php");
+include ("../templates/users_password.php");
+include ("../templates/footer.php");
+
+/* vim: set expandtab softtabstop=4 tabstop=4 shiftwidth=4: */
+?>
