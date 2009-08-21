d8a815c0c8d27241cb8c2c7da7d412e71a0b5a6b
Sebastian2009 <Sebastian2009@a1433add-5e2c-0410-b055-b7f2511e0802>
2009-08-21 07:50:25 +0000

    - get in sync with mainline.
    git-svn-id: svn://svn.code.sf.net/p/postfixadmin/code@709 a1433add-5e2c-0410-b055-b7f2511e0802

--- branches/postfixadmin-smarty/VIRTUAL_VACATION/vacation.pl
+++ branches/postfixadmin-smarty/VIRTUAL_VACATION/vacation.pl
@@ -324,15 +324,18 @@ sub find_real_address {
     my $rv = $stm->rows;
 
 # Recipient has vacation
-    if ($rv == 1) {
-        $realemail = $email;
-        $logger->debug("Found '\$email'\ has vacation active");
-    } else {
-        $logger->debug("Looking for alias records that \'$email\' resolves to with vacation turned on");
-        $query = qq{SELECT goto FROM alias WHERE address=? AND (goto LIKE ? OR goto LIKE % OR goto LIKE ?)};
-        $stm = $dbh->prepare($query) or panic_prepare($query);
-        $stm->execute($email,"$email,%","%,$email","%,$email,%") or panic_execute($query,"address='$email'");
-        $rv = $stm->rows;
+	if ($rv == 1) {
+		$realemail = $email;
+		$logger->debug("Found '\$email'\ has vacation active");
+	} else {
+		my $vemail = $email;
+		$vemail =~ s/\@/#/g;
+		$logger->debug("Looking for alias records that \'$email\' resolves to with vacation turned on");
+		$query = qq{SELECT goto FROM alias WHERE address=? AND (goto LIKE ? OR goto LIKE ? OR goto LIKE ?)};
+		$stm = $dbh->prepare($query) or panic_prepare($query);
+		$stm->execute($email,"$email,%","%,$email","%,$email,%") or panic_execute($query,"address='$email'");
+		$rv = $stm->rows;
+
 
 # Recipient is an alias, check if mailbox has vacation
         if ($rv == 1) { 
