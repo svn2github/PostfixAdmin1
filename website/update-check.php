900456cf444dbadea4b68e9e94be6a7ef542232d
valkum <valkum@a1433add-5e2c-0410-b055-b7f2511e0802>
2010-12-30 17:13:45 +0000

    * Added Wiki to header links * Changed the way the version is compared. New one can handle version over x.10
    git-svn-id: svn://svn.code.sf.net/p/postfixadmin/code@922 a1433add-5e2c-0410-b055-b7f2511e0802

--- website/update-check.php
+++ website/update-check.php
@@ -9,7 +9,7 @@ if(!isset($_GET['version'])) {
 else {
 	$version = $_GET['version'];
 }
-if(strcmp($version,$latest) >= 0) {
+if(version_compare2($version,$latest) >= 0) {
 	echo "Congratulations - you're running the latest version of PostfixAdmin";
 }
 else {
@@ -17,4 +17,30 @@ else {
 }
 
 require_once('footer.php');
+
+
+//Compare two sets of versions, where major/minor/etc. releases are separated by dots.
+//Returns 0 if both are equal, 1 if A > B, and -1 if B < A.
+function version_compare2($a, $b)
+{
+    $a = explode(".", rtrim($a, ".0")); //Split version into pieces and remove trailing .0
+    $b = explode(".", rtrim($b, ".0")); //Split version into pieces and remove trailing .0
+    foreach ($a as $depth => $aVal)
+    { //Iterate over each piece of A
+        if (isset($b[$depth]))
+        { //If B matches A to this depth, compare the values
+            if ($aVal > $b[$depth]) return 1; //Return A > B
+            else if ($aVal < $b[$depth]) return -1; //Return B > A
+            //An equal result is inconclusive at this point
+        }
+        else
+        { //If B does not match A to this depth, then A comes after B in sort order
+            return 1; //so return A > B
+        }
+    }
+    //At this point, we know that to the depth that A and B extend to, they are equivalent.
+    //Either the loop ended because A is shorter than B, or both are equal.
+    return (count($a) < count($b)) ? -1 : 0;
+} 
+
 ?>
